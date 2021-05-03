c82a4e57d8a1773a69ebbb715ca0fdd3fc18e10f
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-03 19:37:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,11 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 	  user = User.find_by(:username => params[:username])
+	  if user
+	    redirect "/success"
+	  else
+	    redirect "/failure"
+	  end
 	end
 
 	get "/success" do
